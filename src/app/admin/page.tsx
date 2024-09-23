fe6fbe5368875767236bf76131bad4f0834c3ec9
CosaNostraa69 <lare.axelange.simplon@gmail.com>
2024-09-24 00:52:54 +0200

    Create Employer && Admin Page with the good permissions

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,8 @@
+export default function AdminPage() {
+    return (
+      <div>
+        <h1>Admin Dashboard</h1>
+        <p>Welcome to the admin area. Only administrators should be able to see this page.</p>
+      </div>
+    )
+  }
---
+++ src/app/employer/page.tsx
@@ -0,0 +1,8 @@
+export default function EmployerPage() {
+    return (
+      <div>
+        <h1>Employer Dashboard</h1>
+        <p>Welcome to the employer area. Only employers should be able to see this page.</p>
+      </div>
+    )
+  }
